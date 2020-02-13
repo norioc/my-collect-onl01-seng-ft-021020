32a1621be2dba09bb1678c635f0f0c7dd6c3dd20
norio c <norionorioc@gmail.com>
2020-02-13 23:23:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++
@@ -1,9 +0,0 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split("").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
